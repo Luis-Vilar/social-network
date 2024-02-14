3214c00d8f18b8e5395b0426c3b9fb8899689b54
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-02-13 23:38:03 -0300

    Agregado módulo de autenticación

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './models/user/user.module';
+import { AuthModule } from './auth/auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
