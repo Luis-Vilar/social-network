013ee5ee7dadc7da953ca1878e20585e5d50433a
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-02-13 23:35:59 -0300

    Añadir controlador de autenticación

---
+++ src/auth/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginUserDto } from '../dto';
+import { ApiTags } from '@nestjs/swagger';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  @ApiTags('App')
+  signIn(@Body() { email, password }: LoginUserDto) {
+    return this.authService.signIn(email, password);
+  }
+}
